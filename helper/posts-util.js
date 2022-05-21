b796077eb4adfa20a8042540f395ca4a97b9726d
VinaySehwag14 <vinaysehwag14@gmail.com>
2022-05-21 23:31:23 +0530

    added matter package and helper file and folder

---
+++ helper/posts-util.js
@@ -0,0 +1,45 @@
+import fs from "fs";
+import matter from "gray-matter";
+import path from "path";
+
+//* path for directory from where we have to read
+const postsDirectory = path.join(process.cwd(), "posts");
+
+function getPostData(fileName) {
+  const filePath = path.join(postsDirectory, fileName);
+  const fileContent = fs.readFileSync(filePath, "utf-8");
+  const { data, content } = matter(fileContent);
+
+  //* removes the file extension
+  const postSlug = fileName.replace(/\.md$/, "");
+
+  const postData = {
+    slug: postSlug,
+    ...data,
+    content: content,
+  };
+
+  return postData;
+}
+
+export function getAllPosts() {
+  //* readdirSync read all content synchronously
+  const postFiles = fs.readdirSync(postsDirectory);
+
+  const allPosts = postFiles.map((postFile) => {
+    return getPostData(postFile);
+  });
+
+  const sortedPosts = allPosts.sort((postA, postB) =>
+    postA.date > postB.date ? -1 : 1
+  );
+  return sortedPosts;
+}
+
+//* function for getting featured posts
+export function getFeaturedPosts() {
+  const allPosts = getAllPosts();
+  const featuredPosts = allPosts.filter((post) => post.isFeatured);
+
+  return featuredPostsa;
+}
