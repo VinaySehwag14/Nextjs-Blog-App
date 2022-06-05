6b02ff7d0dfeceb2955767c88229f1bc79c02406
VinaySehwag14 <vinaysehwag14@gmail.com>
2022-06-05 14:39:32 +0530

    added backend api for storing contact form data in database

---
+++ pages/api/contact.js
@@ -0,0 +1,33 @@
+function handler(req, res) {
+  if (req.method === "POST") {
+    const { email, name, message } = req.body;
+
+    if (
+      !email ||
+      !email.include("@") ||
+      !name ||
+      name.trim() === "" ||
+      !message ||
+      message.trim() === ""
+    ) {
+      res.status(422).json({
+        message: "Invalid input.",
+      });
+      return;
+    }
+
+    //* Store in a database
+
+    const newMessage = {
+      email,
+      name,
+      message,
+    };
+
+    console.log(newMessage);
+
+    res
+      .status(201)
+      .json({ message: "Successfully stored message!", message: newMessage });
+  }
+}
