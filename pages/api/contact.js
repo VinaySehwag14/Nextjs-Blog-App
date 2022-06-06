d2b69109764444c007ab39a1bcf51693ce131e57
VinaySehwag14 <vinaysehwag14@gmail.com>
2022-06-06 19:16:59 +0530

    added mongodb setup and connect with it and modified contact page for notification

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
 
@@ -10,13 +12,9 @@ function handler(req, res) {
       !message ||
       message.trim() === ""
     ) {
-      res.status(422).json({
-        message: "Invalid input.",
-      });
+      res.status(422).json({ message: "Invalid input." });
       return;
     }
-
-    //* Store in a database
 
     const newMessage = {
       email,
@@ -24,7 +22,29 @@ function handler(req, res) {
       message,
     };
 
-    console.log(newMessage);
+    let client;
+
+    try {
+      client = await MongoClient.connect(
+        "mongodb+srv://malang:freewolf@nextblog.t8tra.mongodb.net/?retryWrites=true&w=majority"
+      );
+    } catch (error) {
+      res.status(500).json({ message: "Could not connect to database." });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection("messages").insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: "Storing message failed!" });
+      return;
+    }
+
+    client.close();
 
     res
       .status(201)
