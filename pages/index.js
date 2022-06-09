97ca9794363f79c68d590120f023306691fd9d51
VinaySehwag14 <vinaysehwag14@gmail.com>
2022-06-09 18:23:26 +0530

    added meta tag to pages

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Hero from "../components/HomePage/Hero";
 import FeaturedPosts from "../components/HomePage/FeaturedPosts";
@@ -6,6 +7,13 @@ import { getFeaturedPosts } from "../helper/posts-util";
 const Home = (props) => {
   return (
     <>
+      <Head>
+        <title>Vinay Blog</title>
+        <meta
+          name="description"
+          content="I post about programming and web development"
+        />
+      </Head>
       <Hero />
       <FeaturedPosts posts={props.posts} />
     </>
