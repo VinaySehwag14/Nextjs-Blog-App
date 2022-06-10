b7611611d59cafd5aa161b60dfdfde81585d5d15
VinaySehwag14 <vinaysehwag14@gmail.com>
2022-06-10 17:24:03 +0530

    added env variable during config process

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+  env: {
+    mongodb_username: "malang",
+    mongodb_password: "freewolf",
+    mongodb_clustername: "nextblog",
+  },
+};
--- pages/api/contact.js
+++ pages/api/contact.js
@@ -24,10 +24,10 @@ async function handler(req, res) {
 
     let client;
 
+    const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.t8tra.mongodb.net/?retryWrites=true&w=majority`;
+
     try {
-      client = await MongoClient.connect(
-        "mongodb+srv://malang:freewolf@nextblog.t8tra.mongodb.net/?retryWrites=true&w=majority"
-      );
+      client = await MongoClient.connect(connectionString);
     } catch (error) {
       res.status(500).json({ message: "Could not connect to database." });
       return;
