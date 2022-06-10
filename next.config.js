5796fdb042ee6ec755214195731a1fd386f323fc
VinaySehwag14 <vinaysehwag14@gmail.com>
2022-06-10 17:34:49 +0530

    added next constants for dev or production code

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
-module.exports = {
-  env: {
-    mongodb_username: "malang",
-    mongodb_password: "freewolf",
-    mongodb_clustername: "nextblog",
-  },
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        mongodb_username: "malang",
+        mongodb_password: "freewolf",
+        mongodb_clustername: "nextblog",
+      },
+    };
+  }
+
+  //* this env for production
+  return {
+    env: {
+      mongodb_username: "malang",
+      mongodb_password: "freewolf",
+      mongodb_clustername: "nextblogProd",
+    },
+  };
 };
