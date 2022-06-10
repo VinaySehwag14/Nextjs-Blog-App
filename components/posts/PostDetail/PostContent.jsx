0c1f96f398fd3fc7279d8520e941f2530d7575b5
VinaySehwag14 <vinaysehwag14@gmail.com>
2022-06-10 17:45:27 +0530

    minimised code size by using prismLight highligher

--- components/posts/PostDetail/PostContent.jsx
+++ components/posts/PostDetail/PostContent.jsx
@@ -1,10 +1,22 @@
 import ReactMarkdown from "react-markdown";
 //* for code snippets in blog
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { darcula } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import darcula from "react-syntax-highlighter/dist/cjs/styles/prism/darcula";
+//* language we want to support
+import js from "react-syntax-highlighter/dist/cjs/languages/prism/javascript";
+import css from "react-syntax-highlighter/dist/cjs/languages/prism/css";
+import html from "react-syntax-highlighter/dist/cjs/languages/prism/html";
+import jsx from "react-syntax-highlighter/dist/cjs/languages/prism/jsx";
+
 import PostHeader from "./PostHeader";
 import styles from "./postContent.module.css";
 import Image from "next/image";
+
+//* registering language which we are using is neccessary
+SyntaxHighlighter.registerLanguage("js", js);
+SyntaxHighlighter.registerLanguage("css", css);
+SyntaxHighlighter.registerLanguage("html", html);
+SyntaxHighlighter.registerLanguage("jsx", jsx);
 
 const PostContent = (props) => {
   const { post } = props;
